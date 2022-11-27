fe455e23ab5dfe687773a57fbe4d8585525fe4eb
ly1999-hub <nhly123123456789@gmail.com>
2022-11-27 16:16:29 +0700

    controller attire.js

---
+++ product/controllers/attire.js
@@ -0,0 +1,37 @@
+const Attire = require(".../internal/models/attire");
+const Response=require('.../internal/log/response')
+const { default: mongoose, isValidObjectId } = require('mongoose');
+
+module.exports.getAllAttire = async function(req, res, next) {
+    try {
+        var attires = await Attire.find()
+    } catch (error) {
+        console.log(error)
+    }
+    res.json(attires)
+}
+
+module.exports.getDetailAttire=async function(req,res,next){
+    const id = req.params.id;
+    if (isValidObjectId(id)) {
+        const UserDetail = await Attire.findById(id)
+        res.json(UserDetail)
+    }else{
+        res.json(Response.Status.Status400)
+    }
+}
+
+module.exports.searchAttire=async function(req,res,next){
+    var nameSearch=req.query.name;
+    try {
+        var Attires=await Attire.find();
+        var listAttires=Attires.filter(function(attire){
+            return attire.name.toLowerCase().indexOf(nameSearch.toLowerCase()) !== -1;
+        })
+    
+        res.json(listAttires)
+    } catch (error) {
+        res.json(Response.Status.Status400)
+    }
+    
+}
