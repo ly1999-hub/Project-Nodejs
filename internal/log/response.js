2bdf0f74a0d4d0b9d496428170dd17c2f59b17ce
ly1999-hub <nhly123123456789@gmail.com>
2022-11-28 22:02:27 +0700

    response

---
+++ internal/log/response.js
@@ -0,0 +1,33 @@
+module.exports.Status={
+    Status200:{
+        index:1,
+        code:200,
+        data:''
+    },
+
+    Status400:{
+        index:0,
+        code:400,
+        data:''
+    },
+
+    Status404:{
+        index:0,
+        code:404,
+        data:''
+    },
+
+    Status500:{
+        index:0,
+        code:500,
+        data:''
+    },
+
+    Status503:{
+        index:0,
+        code:503,
+        data:''
+    }
+    
+}
+    
